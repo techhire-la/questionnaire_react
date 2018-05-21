a94d6457fecc227664cf25c8c0fb95730c63c370
Jupiter <jupiterbaudot@gmail.com>
2018-05-21 11:57:52 -0700

    Add FilterForm and QuestionnaireApp components

---
+++ src/components/FilterForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class FilterForm extends React.Component {
+
+
+
+
+    render() {
+
+        return (
+
+            <div>
+
+
+                <h3>Your FilterForm is here</h3>
+
+
+            </div>
+
+    );
+    }
+}
+
+
+export default FilterForm;
--- src/components/QuestionnaireApp.js
+++ src/components/QuestionnaireApp.js
@@ -10,10 +10,24 @@ class QuestionnaireApp extends React.Component {
 
 render() {
 
+    let showNew = ""
+    let showAll = ""
+    if(this.props.toggle){
+        showNew = {display: 'none' }
+        showAll = {display: 'block' }
+    }else{
+        showNew = {display: 'block' }
+        showAll = {display: 'none' }
+    }
+
     return (
 
         <div>
             <h1>Your QuestionnaireApp is here</h1>
+            <button>Form</button>
+            ||
+            <button>Catalogue</button>
+
         </div>
 
         );
