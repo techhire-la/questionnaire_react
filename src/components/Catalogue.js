c698eeadc9b7207d28744d8a95ecbe71cf4b8f90
Jupiter <jupiterbaudot@gmail.com>
2018-05-21 12:52:19 -0700

    Add Catalogue Component

---
+++ src/components/Catalogue.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class Catalogue extends React.Component {
+
+
+
+
+    render() {
+
+        return (
+
+            <div>
+
+
+            <h3>I am your Catalogue</h3>
+
+
+        </div>
+
+    );
+    }
+}
+
+
+export default Catalogue;
