e123225b1b76eefa3c4573da3a4ce9e4f4462ae4
Jupiter <jupiterbaudot@gmail.com>
2018-06-02 12:24:28 -0700

    Validate JSON and add to catalogue

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import catalogueData from "./contacts2.json";
+import catalogueData from "../api/contacts2.json";
 
+var $ = require ('jquery')
 
+// const services = "./api/contacts"
 class Catalogue extends React.Component {
 
+    state = {
+        color: 'blue',
+        services: []
+    }
+
+    componentDidMount = () => {
+        // $.getJSON('/api/contacts.json', (response) => {this.setState({services: response})})
+        // // $.getJSON('./contacts2.json', (response) => {this.setState({services: response})})
+
+        this.setState({ services: catalogueData})
+        console.log(this.state.services)
+        // for(var i = 0; i < catalogueData.length; i++) {
+        //
+        //     var obj = catalogueData[i];
+        //
+        //     console.log("Name: " + obj.name + ", " + obj.zip);
+        // }
+    }
 
 
 
     render() {
+
+        console.log(this.state.services)
+        console.log(this.state.color)
 
         return (
 
@@ -18,9 +43,9 @@ class Catalogue extends React.Component {
             <h3>I am your Catalogue</h3>
 
 
-        </div>
+            </div>
 
-    );
+        );
     }
 }
 
