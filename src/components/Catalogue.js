edcf0ed528d7df38707d979a0729e72d89d4fe25
Jupiter <jupiterbaudot@gmail.com>
2018-06-04 17:01:22 -0700

    Add mapping functionality to individual programs in the catalogue and display in the view

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Program from './Program';
 
 import catalogueData from "../api/contacts.json";
 
 
 class Catalogue extends React.Component {
 
+// > programs.contacts[i].name
+//     'YouthWorks'
+// > programs.contacts[i].email
+//     'mhernandez@ypiusa.org'
+// > programs.contacts[i].phone
+
     state = {
         programs: []
     }
 
-    componentDidMount = () => {
+    componentWillMount = () => {
 
         this.setState({ programs: catalogueData})
         console.log(this.state.programs)
 
-        // for(var i = 0; i < catalogueData.length; i++) {
-        //
-        //     var obj = catalogueData[i];
-        //
-        //     console.log("Name: " + obj.name + ", " + obj.zip);
-        // }
+    // for(var i = 0; i < catalogueData.length; i++) {
+    //
+    //     var obj = catalogueData[i];
+    //
+    //     console.log("Name: " + obj.name + ", " + obj.zip);
+    // }
+
+    }
+
+    setInitialState = () => {
+
+        this.setState({ programs: catalogueData})
+        console.log(this.state.programs)
 
     }
 
@@ -28,20 +42,35 @@ class Catalogue extends React.Component {
 
     render() {
 
-        console.log(this.state.services)
+        console.log(this.state.programs);
+        var programs = this.state.programs.contacts;
+        console.log("programs: " + programs);
+        // debugger
+
 
         return (
 
-            <div>
+                <div className="">
+
+                    <div className="">
+                        {
+                            programs.map((program, index) => (
+                                <Program
+                                    key={index}
+                                    programName={program.name}
+                                    programEmail={program.email}
+                                    programPhone={program.phone}
+                                    count={index + 1}
+                                />
+                            ))
+                        }
+                    </div>
 
 
-            <h3>I am your Catalogue</h3>
+                </div>
 
-
-            </div>
-
-        );
-    }
+            );
+        }
 }
 
 
--- src/components/Program.js
+++ src/components/Program.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+
+const Program = (props) => (
+
+    <div className="">
+        <p className=""> ({props.count}) {props.programName} | {props.programEmail} | {props.programPhone} </p>
+    </div>
+
+);
+
+
+
+export default Program;
