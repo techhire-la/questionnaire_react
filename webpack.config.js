19a1f8fc0783589b497cb268a26b159e75aed620
Jupiter <jupiterbaudot@gmail.com>
2018-06-25 19:05:44 -0700

    Add Background

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
         'css-loader',
         'sass-loader'
       ]
-    }]
+    }, { 
+      test: /\.(png|jpg)$/, 
+      loader: 'url-loader'
+     }]
+
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
