ae35223e78265d8efffeca8a9b107d0a2b5d19e3
Jupiter <jupiterbaudot@gmail.com>
2018-06-27 14:11:53 -0700

    Styles added home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => (
         <div className="header__container">
             <h1 className="header__title">YPI Referral Tool</h1>
             <NavLink className="header__subtitle" to="/" activeClassName="is-active" exact={true}>Home</NavLink> |
-            <NavLink className="header__subtitle" to="/form" activeClassName="is-active">Form</NavLink> |
-            <NavLink className="header__subtitle" to="/catalogue" activeClassName="is-active">Catalogue</NavLink>
+            <NavLink className="header__subtitle" to="/form" activeClassName="is-active"> Questions </NavLink> |
+            <NavLink className="header__subtitle" to="/catalogue" activeClassName="is-active"> Catalogue</NavLink>
         </div>
 
     </div>
--- src/components/QuestionnaireApp.js
+++ src/components/QuestionnaireApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Image, Item, Responsive, Segment , Form, Button, } from 'semantic-ui-react'
+import { Grid, Image, Item, Responsive, Segment , Form, Button, } from 'semantic-ui-react'
 
 import { Link, NavLink} from 'react-router-dom'
 
@@ -26,31 +26,46 @@ render() {
 
         <div className="ui filterContainer">
 
-            <Form>
-                    <Form.Group widths='equal'>
+            <h2>Welcome to the YPI Referral Tool</h2>
+            <hr/>
+
+            <p>The purpose of this app is to help find services most relevant to our clients and to put them in touch with point of contacts within the program.</p>
+
+            <Grid columns={12}>
+                <Grid.Row>
+                    <Grid.Column width={11}>
+                        <p>If you are uncertain which programs may pertain to you, you can fill out our questions form that will filter relevant results</p>
+                    </Grid.Column>
+
+                    <Grid.Column className="pushLeft" centered width={1}>
                         <Link to="/form">
                             <Button secondary size='massive'>
-                                <p>Form</p>
+                                <p>Questions</p>
                             </Button>
                         </Link>
+                    </Grid.Column>
 
+                </Grid.Row>
+            </Grid>
+
+            <Grid columns={12}>
+                <Grid.Row>
+                    <Grid.Column width={11}>
+                        <p>If you are familiar with our services and would like to view our full list of YPI programs, check out our catalogue</p>
+                    </Grid.Column>
+
+                    <Grid.Column className="pushLeft" width={1}>
                         <Link to="/catalogue">
                             <Button secondary size='massive'>
                                 <p>Catalogue</p>
                             </Button>
                         </Link>
-                    </Form.Group>
+                    </Grid.Column>
+
+                </Grid.Row>
+            </Grid>
 
 
-
-                    <Form.Group widths='equal'>
-
-                    </Form.Group>
-            </Form>
-
-            <div className="center">
-
-            </div>
 
 
         </div>
@@ -65,3 +80,31 @@ export default QuestionnaireApp;
 
 
 // <Button style={myStyle}>
+
+
+{/*<Form>*/}
+    {/*<Form.Group widths='equal'>*/}
+
+        {/*<Link to="/form">*/}
+            {/*<Button secondary size='massive'>*/}
+                {/*<p>Questions Form</p>*/}
+            {/*</Button>*/}
+        {/*</Link>*/}
+
+        {/*<Link to="/catalogue">*/}
+            {/*<Button secondary size='massive'>*/}
+                {/*<p>Catalogue</p>*/}
+            {/*</Button>*/}
+        {/*</Link>*/}
+    {/*</Form.Group>*/}
+
+
+
+    {/*<Form.Group widths='equal'>*/}
+
+    {/*</Form.Group>*/}
+{/*</Form>*/}
+
+{/*<div className="center">*/}
+
+    {/*</div>*/}
