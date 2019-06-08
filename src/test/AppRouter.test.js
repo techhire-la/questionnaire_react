41835488213c0cea287e92234546fc5ded906c31
Andy <andy.wm.arthur@gmail.com>
2019-06-07 21:26:18 -0700

    simple enzyme test working

--- src/test/AppRouter.test.js
+++ src/test/AppRouter.test.js
@@ -1,7 +1,9 @@
-describe('Examining the syntax of Jest tests', () => {
-   
-    it('sums numbers', () => {
-        expect(1 + 2).toEqual(3);
-        expect(2 + 2).toEqual(4);
-     });
-  });
+import React from 'react';
+import { shallow } from 'enzyme';
+import AppRouter from '../routes/AppRouter';
+
+describe('AppRouter', () => {
+   it('renders without crashing', () => {
+      shallow(<AppRouter />);
+    });
+});
