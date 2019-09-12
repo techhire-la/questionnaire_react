a72e84adc6e0147ef097e3b12b207e300dc1e51c
Jupiter <jupiterbaudot@gmail.com>
2019-09-11 18:10:51 -0700

    Add alert reducer functionality

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,4 +2,16 @@
 
 const initialState = []
 
-// export default 
+export default function(state=initialState, action){
+    //extract attributes from the action param
+    const {type, payload} = action
+
+    switch(type){
+        case SET_ALERT:
+            return [...state, payload];
+        case REMOVE_ALERT:
+            return state.filter(alert => alert.id !== payload);
+        default:
+            return state;
+    }
+}
